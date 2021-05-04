c7ca1027c9cd83a18ab3f8d36dfb013b0355acb8
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-05-04 10:46:12 +0300

    Initial Commit

---
+++ sama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sama
+//
+//  Created by Viktoras Laukevičius on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
