7b90541ed8da11c55cde4f0ef919a3932262bfc1
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-05-13 11:11:17 +0300

    Handle token received via custom scheme & store it

--- sama/Auth/AuthToken.swift
+++ sama/Auth/AuthToken.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct AuthToken: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
--- sama/ViewController.swift
+++ sama/ViewController.swift
@@ -10,9 +10,21 @@ import AuthenticationServices
 
 class ViewController: UIViewController, ASWebAuthenticationPresentationContextProviding {
 
+    @IBOutlet private var connectButton: UIButton!
+    @IBOutlet private var disconnectButton: UIButton!
+    @IBOutlet private var fetchCalendarButton: UIButton!
+
+    private var isLoggedIn = false {
+        didSet {
+            connectButton.isHidden = isLoggedIn
+            disconnectButton.isHidden = !isLoggedIn
+            fetchCalendarButton.isHidden = !isLoggedIn
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        isLoggedIn = UserDefaults.standard.data(forKey: "SAMA_AUTH_TOKEN") != nil
     }
 
     @IBAction func onConnectCalendar(_ sender: Any) {
@@ -28,10 +40,39 @@ class ViewController: UIViewController, ASWebAuthenticationPresentationContextPr
         }.resume()
     }
 
+    @IBAction func onDisconnectCalendar(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: "SAMA_AUTH_TOKEN")
+        isLoggedIn = false
+    }
+
+    @IBAction func onFetchCalendar(_ sender: Any) {
+        guard
+            let tokenData = UserDefaults.standard.data(forKey: "SAMA_AUTH_TOKEN"),
+            let token = try? JSONDecoder().decode(AuthToken.self, from: tokenData)
+        else { return }
+        var req = URLRequest(url: URL(string: "https://app.yoursama.com/api/calendar")!)
+        req.httpMethod = "get"
+        req.setValue("Bearer \(token.accessToken)", forHTTPHeaderField: "Authorization")
+        URLSession.shared.dataTask(with: req) { (data, resp, err) in
+            print("")
+        }.resume()
+    }
+
     private func authenticate(with url: String) {
         let session = ASWebAuthenticationSession(url: URL(string: url)!, callbackURLScheme: "yoursama") { (callbackUrl, err) in
-            print(callbackUrl)
-            print(err)
+            guard
+                let url = callbackUrl,
+                url.scheme == "yoursama",
+                url.host == "auth",
+                url.path == "/success",
+                let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems,
+                let accessToken = queryItems.first(where: { $0.name == "accessToken" })?.value,
+                let refreshToken = queryItems.first(where: { $0.name == "refreshToken" })?.value
+            else { return }
+
+            let token = AuthToken(accessToken: accessToken, refreshToken: refreshToken)
+            UserDefaults.standard.set(try? JSONEncoder().encode(token), forKey: "SAMA_AUTH_TOKEN")
+            self.isLoggedIn = true
         }
         session.presentationContextProvider = self
         session.start()
