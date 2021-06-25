14b20d9cabca62a07be6c064746b864ae409b9b8
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-06-25 14:25:20 +0300

    Display dots in event view

--- sama/Calendar/EventView.swift
+++ sama/Calendar/EventView.swift
@@ -12,18 +12,44 @@ class EventView: UIView {
     private var isReady = false
     private var backgroundLayer: CALayer?
 
+    private let dotsView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        dotsView.backgroundColor = UIColor(patternImage: UIImage(named: "event-bg-dot")!)
+        addSubview(dotsView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private var innerFrameWidth: CGFloat {
+        bounds.width - eventRightBottomInset.x
+    }
+
     override func layoutSubviews() {
         super.layoutSubviews()
         if !isReady {
             isReady = true
             setupLayers()
         }
+        reframeDotsView()
+    }
+
+    private func reframeDotsView() {
+        let space = innerFrameWidth - 2 * 6
+        let dots = floor(space / 8)
+        let width = dots * 8
+        let x = (innerFrameWidth - width) / 2
+        dotsView.frame = CGRect(x: x, y: 6, width: width, height: 16)
     }
 
     private func setupLayers() {
         let middle = CGPoint(x: bounds.midX - (eventRightBottomInset.x / 2), y: bounds.midY - (eventRightBottomInset.y / 2))
 
-        let innerFrame = CGRect(x: 0, y: 0, width: bounds.width - eventRightBottomInset.x, height: bounds.height - eventRightBottomInset.y)
+        let innerFrame = CGRect(x: 0, y: 0, width: innerFrameWidth, height: bounds.height - eventRightBottomInset.y)
         let shadow = CALayer()
         shadow.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: 8).cgPath
         shadow.shadowColor = UIColor(red: 0.467, green: 0.134, blue: 0.056, alpha: 0.15).cgColor
