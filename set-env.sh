817e42f55001d05ae13588b8af05388818a8de48
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-07-15 23:07:08 +0300

    Build dir hierarchy before copy

--- set-env.sh
+++ set-env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
 type=$1
 
 if [ "$type" = "prod" ] || [ "$type" == "dev" ]; then
   echo "Configuring environment for $type..."
   cp -rf "env/$type/GoogleService-Info.plist" "sama/GoogleService-Info.plist"
   cp -rf "env/$type/sama.xcconfig" "sama/Resources/sama.xcconfig"
+  mkdir -p "sama/Environment"
   cp -rf "env/$type/SamaKeys.swift" "sama/Environment/SamaKeys.swift"
   cp -rf "env/$type/AppIcon.appiconset" "sama/Assets.xcassets/AppIcon.appiconset"
   exit 0
