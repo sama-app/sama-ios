e3abcc33555f87b9ee57314b73a3a1eac71a11bb
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-07-16 20:50:53 +0300

    Clean up CI config

--- build-distribute.sh
+++ build-distribute.sh
@@ -10,6 +10,4 @@ rm -rf $archivePath
 set -o pipefail && xcodebuild -project sama.xcodeproj -scheme sama -configuration Release -archivePath $archivePath archive CODE_SIGN_STYLE=Manual | xcpretty
 set -o pipefail && xcodebuild -exportArchive -archivePath $archivePath -exportOptionsPlist exportOptions.plist -exportPath $buildPath | xcpretty
 # distribute
-xcrun altool --validate-app --type ios --file "$buildPath/Sama.ipa" --apiKey "$APPSTORE_API_KEY" --apiIssuer "$APPSTORE_API_ISSUER"
-echo "Trying to upload"
 xcrun altool --upload-app --type ios --file "$buildPath/Sama.ipa" --apiKey "$APPSTORE_API_KEY" --apiIssuer "$APPSTORE_API_ISSUER"
