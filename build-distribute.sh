1f96841324d1febbd1c153ef89e13e3b20d6bcda
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-07-16 21:29:49 +0300

    Diff exportOptions.plist per env

--- build-distribute.sh
+++ build-distribute.sh
@@ -8,6 +8,6 @@ rm -rf $buildPath
 rm -rf $archivePath
 # build and archive
 set -o pipefail && xcodebuild -project sama.xcodeproj -scheme sama -configuration Release -archivePath $archivePath archive CODE_SIGN_STYLE=Manual | xcpretty
-set -o pipefail && xcodebuild -exportArchive -archivePath $archivePath -exportOptionsPlist exportOptions.plist -exportPath $buildPath | xcpretty
+set -o pipefail && xcodebuild -exportArchive -archivePath $archivePath -exportOptionsPlist $EXPORT_OPTIONS_PLIST -exportPath $buildPath | xcpretty
 # distribute
 xcrun altool --upload-app --type ios --file "$buildPath/Sama.ipa" --apiKey "$APPSTORE_API_KEY" --apiIssuer "$APPSTORE_API_ISSUER"
