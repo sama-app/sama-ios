8729b9d89c2a05e6c78a5eda04831356ee67e1be
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-07-16 13:27:26 +0300

    Validate artifact & validate before uploading

--- build-distribute.sh
+++ build-distribute.sh
@@ -10,4 +10,6 @@ rm -rf $archivePath
 set -o pipefail && xcodebuild -project sama.xcodeproj -scheme sama -configuration Release -archivePath $archivePath archive CODE_SIGN_STYLE=Manual | xcpretty
 set -o pipefail && xcodebuild -exportArchive -archivePath $archivePath -exportOptionsPlist exportOptions.plist -exportPath $buildPath | xcpretty
 # distribute
-# xcrun altool --upload-app --type ios --file "$buildPath/Sama.ipa" --apiKey $APPSTORE_API_KEY --apiIssuer $APPSTORE_API_ISSUER
+xcrun altool --validate-app --type ios --file "$buildPath/Sama.ipa" --apiKey "$APPSTORE_API_KEY" --apiIssuer "$APPSTORE_API_ISSUER"
+echo "Trying to upload"
+xcrun altool --upload-app --type ios --file "$buildPath/Sama.ipa" --apiKey "$APPSTORE_API_KEY" --apiIssuer "$APPSTORE_API_ISSUER"
