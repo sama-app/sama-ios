5d1d50d8570c49e49a065d1545fd1859923c2bec
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-07-27 14:42:45 +0300

    Disable analytics for debug

--- sama/AppDelegate.swift
+++ sama/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         FirebaseApp.configure()
         #if DEBUG
         Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(false)
+        Analytics.setAnalyticsCollectionEnabled(false)
         #endif
         Messaging.messaging().isAutoInitEnabled = false
         Messaging.messaging().delegate = self
