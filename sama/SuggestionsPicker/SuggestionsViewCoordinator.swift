96e20311c2eb9335637fd3869cff3fa154f8a7b3
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-08-22 19:49:25 +0300

    Extract meeting proposal data request handling

--- sama/SuggestionsPicker/SuggestionsViewCoordinator.swift
+++ sama/SuggestionsPicker/SuggestionsViewCoordinator.swift
@@ -60,7 +60,7 @@ class SuggestionsViewCoordinator {
     // pin to 15 mins
     private let hourSplit = 4
 
-    private let apiDateF = ApiDateTimeFormatter()
+    private let transformer = ProposedAvailableSlotsTransformer()
 
     private var selectionIndex = 0 {
         didSet {
@@ -97,73 +97,13 @@ class SuggestionsViewCoordinator {
     func present() {
         api.request(for: MeetingProposalsRequest(code: "EWOdWQ5C")) {
             switch $0 {
-            case let .success(proposals):
-                let calendar = Calendar.current
-                let refDate = Date()
-                let rawSlots: [ProposedAvailableSlot] = proposals.proposedSlots.map { slot in
-                    let parsedStart = self.apiDateF.date(from: slot.startDateTime)
-                    let startDate = calendar.toTimeZone(date: parsedStart)
-                    let parsedEnd = self.apiDateF.date(from: slot.endDateTime)
-                    let endDate = calendar.toTimeZone(date: parsedEnd)
-                    let startInDay = startDate.timeIntervalSince(calendar.startOfDay(for: startDate))
-                    let durationVal = endDate.timeIntervalSince(startDate)
-                    let duration = NSDecimalNumber(value: durationVal).dividing(by: NSDecimalNumber(value: 3600)).decimalValue
-                    let start = NSDecimalNumber(value: startInDay).dividing(by: NSDecimalNumber(value: 3600)).decimalValue
-                    let daysOffset = calendar.dateComponents(
-                        [.day],
-                        from: calendar.startOfDay(for: refDate),
-                        to: calendar.startOfDay(for: startDate)
-                    ).day!
-                    return ProposedAvailableSlot(
-                        start: start,
-                        duration: duration,
-                        daysOffset: daysOffset,
-                        pickStart: start
-                    )
-                }
+            case let .success(rawProposal):
+                let proposal = self.transformer.transform(proposal: rawProposal, calendar: .current, refDate: Date())
 
-                // merging slots
-                var mergedSlots: [ProposedAvailableSlot] = []
-                for slot in rawSlots {
-                    var isMerged = false
-                    for (idx, finalSlot) in mergedSlots.enumerated() {
-                        if slot.daysOffset == finalSlot.daysOffset && slot.start >= finalSlot.start && slot.start < (finalSlot.start + finalSlot.duration) {
-                            let end = slot.start + slot.duration
-                            let duration = end - finalSlot.start
+                self.duration = proposal.duration
+                self.availableSlotProps = proposal.slots
 
-                            mergedSlots[idx] = ProposedAvailableSlot(
-                                start: finalSlot.start,
-                                duration: duration,
-                                daysOffset: finalSlot.daysOffset,
-                                pickStart: finalSlot.pickStart
-                            )
-
-                            isMerged = true
-                            break
-                        }
-                    }
-                    if !isMerged {
-                        mergedSlots.append(slot)
-                    }
-                }
-                mergedSlots.sort(by: {
-                    switch true {
-                    case $0.daysOffset < $1.daysOffset:
-                        return true
-                    case $0.daysOffset == $1.daysOffset:
-                        return $0.start < $1.start
-                    default:
-                        return false
-                    }
-                })
-
-                if let firstSlot = rawSlots.first {
-                    // first slot defines duration
-                    self.duration = firstSlot.duration
-                }
-                self.availableSlotProps = mergedSlots
-
-                self.availableSlotViews = mergedSlots.enumerated().map { idx, slot in
+                self.availableSlotViews = self.availableSlotProps.enumerated().map { idx, slot in
                     let v = SlotSuggestionView()
 
                     let tapHandler = UITapGestureRecognizer(target: self, action: #selector(self.handleSlotTap))
