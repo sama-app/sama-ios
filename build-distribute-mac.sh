90521d979faa2a1396c223836b3fa92857ccaff4
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-09-15 20:17:27 +0300

    Introduce mac distribution script

---
+++ build-distribute-mac.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+set -e
+
+archivePath='sama.xcarchive'
+buildPath='build-sama'
+
+# clean
+rm -rf $buildPath
+rm -rf $archivePath
+
+# build and archive
+set -o pipefail && \
+ xcodebuild \
+ -project sama.xcodeproj \
+ -scheme sama-mac \
+ -configuration Release \
+ -archivePath $archivePath \
+ archive CODE_SIGN_STYLE=Manual \
+ | xcpretty
+set -o pipefail && \
+ xcodebuild -exportArchive \
+ -archivePath $archivePath \
+ -exportOptionsPlist "./env/dev/exportOptions-mac.plist" \
+ -exportPath $buildPath \
+ | xcpretty
+
+# notarize
+
+appPath="$buildPath/Sama.app"
+appZipPath="$buildPath/Sama.zip"
+notarizationRequestPath="$buildPath/notarization-request.plist"
+notarizationResultPath="$buildPath/notarization-result.plist"
+
+ditto -c -k --keepParent $appPath $appZipPath
+
+xcrun altool \
+ --notarize-app \
+ --primary-bundle-id "com.meetsama.app.dev" \
+ --apiKey "$APPSTORE_API_KEY" \
+ --apiIssuer "$APPSTORE_API_ISSUER" \
+ -f $appZipPath \
+ --output-format xml \
+ > $notarizationRequestPath
+
+while true; do
+  xcrun altool \
+    --notarization-info `/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" $notarizationRequestPath` \
+    --apiKey "$APPSTORE_API_KEY" \
+    --apiIssuer "$APPSTORE_API_ISSUER" \
+    --output-format xml > $notarizationResultPath
+  status=`/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" $notarizationResultPath`
+  if [ status != "in progress" ]; then
+      break;
+  fi;
+  sleep 60
+done
+
+xcrun stapler staple $appPath
+
+# distribute
+# TODO
+
