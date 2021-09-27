71eb0c76a1bfa1a6fbd6dc58b37610f5e1f29b33
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-09-27 21:16:56 +0300

    Remove old files when configuring environment

--- set-env.sh
+++ set-env.sh
@@ -2,14 +2,30 @@
 set -e
 type=$1
 
+ENV_PATH="sama/Environment"
+
+GOOGLE_SERVICE_PATH="sama/GoogleService-Info.plist"
+XCCONFIG_PATH="sama/Resources/sama.xcconfig"
+KEYS_PATH="$ENV_PATH/SamaKeys.swift"
+APP_ICON_PATH="sama/Assets.xcassets/AppIcon.appiconset"
+ENTITLEMENTS_PATH="sama/sama.entitlements"
+
+rm -f $GOOGLE_SERVICE_PATH
+rm -f $XCCONFIG_PATH
+rm -f $KEYS_PATH
+rm -r -f $APP_ICON_PATH
+rm -f $ENTITLEMENTS_PATH
+
+SRC_PATH="env/$type"
+
 if [ "$type" = "prod" ] || [ "$type" == "dev" ]; then
   echo "Configuring environment for $type..."
-  cp -rf "env/$type/GoogleService-Info.plist" "sama/GoogleService-Info.plist"
-  cp -rf "env/$type/sama.xcconfig" "sama/Resources/sama.xcconfig"
-  mkdir -p "sama/Environment"
-  cp -rf "env/$type/SamaKeys.swift" "sama/Environment/SamaKeys.swift"
-  cp -rf "env/$type/AppIcon.appiconset" "sama/Assets.xcassets/AppIcon.appiconset"
-  cp -rf "env/$type/sama.entitlements" "sama/sama.entitlements"
+  cp -rf "$SRC_PATH/GoogleService-Info.plist" $GOOGLE_SERVICE_PATH
+  cp -rf "$SRC_PATH/sama.xcconfig" $XCCONFIG_PATH
+  mkdir -p $ENV_PATH
+  cp -rf "$SRC_PATH/SamaKeys.swift" $KEYS_PATH
+  cp -rf "$SRC_PATH/AppIcon.appiconset" $APP_ICON_PATH
+  cp -rf "$SRC_PATH/sama.entitlements" $ENTITLEMENTS_PATH
   exit 0
 fi
 
