90cb306d368d5bd15c40584334046dc4b23bab46
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-10-05 10:24:17 +0300

    Script build and distribute mac app to appstore

---
+++ build-distribute-mac-appstore.sh
@@ -0,0 +1,29 @@
+archivePath='sama.xcarchive'
+buildPath='build-sama'
+
+# clean
+rm -rf $buildPath
+rm -rf $archivePath
+
+# build and archive
+set -o pipefail && \
+ xcodebuild \
+ -project sama.xcodeproj \
+ -scheme sama-mac \
+ -configuration Release \
+ -archivePath $archivePath \
+ archive CODE_SIGN_STYLE=Manual \
+ | xcpretty
+set -o pipefail && \
+ xcodebuild -exportArchive \
+ -archivePath $archivePath \
+ -exportOptionsPlist "./env/prod/exportOptions-mac-appstore.plist" \
+ -exportPath $buildPath \
+ | xcpretty
+# distribute
+xcrun altool \
+ --upload-app \
+ --type osx \
+ --file "$buildPath/Sama.pkg" \
+ --apiKey "$APPSTORE_API_KEY" \
+ --apiIssuer "$APPSTORE_API_ISSUER"
