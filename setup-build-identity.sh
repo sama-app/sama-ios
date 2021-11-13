6d69ffa78b6f53f57e93cbb4cce418f9f654ae93
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-11-13 18:31:27 +0200

    Copy mac pp

--- setup-build-identity.sh
+++ setup-build-identity.sh
@@ -33,6 +33,6 @@ security list-keychain -d user -s $KEYCHAIN_PATH
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp $BUILD_PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
 build_mac_pp_uuid=`grep UUID -A1 -a $BUILD_MAC_PP_PATH | grep -io "[-A-F0-9]\{36\}"`
-cp $BUILD_MAC_PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
-mv "~/Library/MobileDevice/Provisioning Profiles/build_mac_pp.mobileprovision" "~/Library/MobileDevice/Provisioning Profiles/$build_mac_pp_uuid.mobileprovision"
+# cp $BUILD_MAC_PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
+mv $BUILD_MAC_PP_PATH "~/Library/MobileDevice/Provisioning Profiles/$build_mac_pp_uuid.mobileprovision"
 # cp $DEV_ID_PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
