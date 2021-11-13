11d1f1a88e1c7121066d78367e5d3fe2167c101c
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-11-13 19:12:37 +0200

    Store pre-notarized app zip

--- build-distribute-mac.sh
+++ build-distribute-mac.sh
@@ -24,6 +24,8 @@ set -o pipefail && \
  -exportPath $buildPath \
  | xcpretty
 
+echo "finish export"
+
 # notarize
 
 appPath="$buildPath/Sama.app"
@@ -32,33 +34,8 @@ appZipPath="$buildPath/Sama.zip"
 notarizationRequestPath="$buildPath/notarization-request.plist"
 notarizationResultPath="$buildPath/notarization-result.plist"
 
+echo "zip pre notarization"
 ditto -c -k --keepParent $appPath $appPreNotarizationZipPath
-
-xcrun altool \
- --notarize-app \
- --primary-bundle-id "com.meetsama.app.dev" \
- --apiKey "$APPSTORE_API_KEY" \
- --apiIssuer "$APPSTORE_API_ISSUER" \
- -f $appPreNotarizationZipPath \
- --output-format xml \
- > $notarizationRequestPath
-
-while true; do
-  xcrun altool \
-    --notarization-info `/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" $notarizationRequestPath` \
-    --apiKey "$APPSTORE_API_KEY" \
-    --apiIssuer "$APPSTORE_API_ISSUER" \
-    --output-format xml > $notarizationResultPath
-  status=`/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" $notarizationResultPath`
-  if [ "$status" != "in progress" ]; then
-    break;
-  fi;
-  sleep 10
-done
-
-xcrun stapler staple $appPath
-
-ditto -c -k --keepParent $appPath $appZipPath
 
 # distribute
 # TODO
