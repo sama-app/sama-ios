44fdd6db76022380841c9c23ab19df9480d79af8
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-11-14 12:24:58 +0200

    Download pre-notarized app artifact and notarize

--- build-distribute-mac.sh
+++ build-distribute-mac.sh
@@ -5,26 +5,28 @@ archivePath='sama.xcarchive'
 buildPath='build-sama'
 
 # clean
-rm -rf $buildPath
-rm -rf $archivePath
+# rm -rf $buildPath
+# rm -rf $archivePath
 
-# build and archive
-set -o pipefail && \
- xcodebuild \
- -project sama.xcodeproj \
- -scheme sama-mac \
- -configuration Release \
- -archivePath $archivePath \
- archive CODE_SIGN_STYLE=Manual \
- | xcpretty
-set -o pipefail && \
- xcodebuild -exportArchive \
- -archivePath $archivePath \
- -exportOptionsPlist $EXPORT_OPTIONS_PLIST \
- -exportPath $buildPath \
- | xcpretty
+mkdir buildPath
 
-echo "finish export"
+cp Sama-pre-notarization.zip build-sama/Sama-pre-notarization.zip
+
+# # build and archive
+# set -o pipefail && \
+#  xcodebuild \
+#  -project sama.xcodeproj \
+#  -scheme sama-mac \
+#  -configuration Release \
+#  -archivePath $archivePath \
+#  archive CODE_SIGN_STYLE=Manual \
+#  | xcpretty
+# set -o pipefail && \
+#  xcodebuild -exportArchive \
+#  -archivePath $archivePath \
+#  -exportOptionsPlist $EXPORT_OPTIONS_PLIST \
+#  -exportPath $buildPath \
+#  | xcpretty
 
 # notarize
 
@@ -34,8 +36,38 @@ appZipPath="$buildPath/Sama.zip"
 notarizationRequestPath="$buildPath/notarization-request.plist"
 notarizationResultPath="$buildPath/notarization-result.plist"
 
-echo "zip pre notarization"
-ditto -c -k --keepParent $appPath $appPreNotarizationZipPath
+# echo "zip pre notarization"
+# ditto -c -k --keepParent $appPath $appPreNotarizationZipPath
+
+echo "notarize"
+xcrun altool \
+ --notarize-app \
+ --primary-bundle-id "com.meetsama.app.dev" \
+ --apiKey "$APPSTORE_API_KEY" \
+ --apiIssuer "$APPSTORE_API_ISSUER" \
+ -f $appPreNotarizationZipPath \
+ --output-format xml \
+ > $notarizationRequestPath
+
+echo "wait for result"
+while true; do
+  xcrun altool \
+    --notarization-info `/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" $notarizationRequestPath` \
+    --apiKey "$APPSTORE_API_KEY" \
+    --apiIssuer "$APPSTORE_API_ISSUER" \
+    --output-format xml > $notarizationResultPath
+  status=`/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" $notarizationResultPath`
+  if [ "$status" != "in progress" ]; then
+    break;
+  fi;
+  sleep 10
+done
+
+echo "staple"
+xcrun stapler staple $appPath
+
+echo "zip notarized"
+ditto -c -k --keepParent $appPath $appZipPath
 
 # distribute
 # TODO
