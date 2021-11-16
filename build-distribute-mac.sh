7d7499330bf884064d5d80c0b9e48415891a222f
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-11-17 00:23:48 +0200

    Read raw notarization log, save app zip and send to slack

--- build-distribute-mac.sh
+++ build-distribute-mac.sh
@@ -29,8 +29,8 @@ set -o pipefail && \
 appPath="$buildPath/Sama.app"
 appPreNotarizationZipPath="$buildPath/Sama-pre-notarization.zip"
 appZipPath="$buildPath/Sama.zip"
-notarizationRequestPath="$buildPath/notarization-request.plist"
-notarizationResultPath="$buildPath/notarization-result.plist"
+notarizationRequestPath="$buildPath/notarization-request"
+notarizationResultPath="$buildPath/notarization-result"
 
 echo "zip pre notarization"
 ditto -c -k --keepParent $appPath $appPreNotarizationZipPath
@@ -41,31 +41,26 @@ xcrun altool \
  --primary-bundle-id "com.meetsama.app.dev" \
  --apiKey "$APPSTORE_API_KEY" \
  --apiIssuer "$APPSTORE_API_ISSUER" \
- -f $appPreNotarizationZipPath \
- --output-format xml \
- > $notarizationRequestPath
+ -f $appPreNotarizationZipPath > $notarizationRequestPath
 
-echo "wait for result"
-# requestUUID=`/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" $notarizationRequestPath`
-# echo $requestUUID
-# while true; do
-#   xcrun altool \
-#     --notarization-info "$requestUUID" \
-#     --apiKey "$APPSTORE_API_KEY" \
-#     --apiIssuer "$APPSTORE_API_ISSUER" \
-#     --output-format xml > $notarizationResultPath
-#   status=`/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" $notarizationResultPath`
-#   if [ "$status" != "in progress" ]; then
-#     break;
-#   fi;
-#   sleep 10
-# done
+echo "wait for notarization result"
+requestUUID=$(awk -F ' = ' '/RequestUUID/ {print $2}' "$notarizationRequestPath")
+while true; do
+  xcrun altool \
+    --notarization-info "$requestUUID" \
+    --apiKey "$APPSTORE_API_KEY" \
+    --apiIssuer "$APPSTORE_API_ISSUER" > $notarizationResultPath
+  if ! grep -q "Status: in progress" "$notarizationResultPath"; then
+    break;
+  fi
+  sleep 10
+done
 
-# echo "staple"
-# xcrun stapler staple $appPath
+echo "staple"
+xcrun stapler staple $appPath
 
-# echo "zip notarized"
-# ditto -c -k --keepParent $appPath $appZipPath
+echo "zip notarized"
+ditto -c -k --keepParent $appPath $appZipPath
 
 # distribute
 # TODO
